6f2383f428564a298e9bcef5327645cf62f3ddb6
minho <jomin981007@naver.com>
2023-06-14 14:35:53 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("only one");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
@@ -13,3 +20,6 @@ export default App;
 
 //컴포넌트를 다르게 만들고, 그 컴포넌트를 위한 css를 만들고, className을 기억할 필요 없음.
 // 왜? => className={styles.title} 이런식으로 필요할 때 써주면 랜덤으로 만들어주니까!
+//코드를 딱 한 번 실행할 거임. 뭘?
+//useEffect는 두 개의 argument를 가짐.
+//useEffect는 우리 코드가 딱 한 번만 실행될 수 있도록 보호해줌.
